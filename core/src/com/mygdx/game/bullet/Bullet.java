3a989d72797ecabf29d944c5c5dd3853101ccb52
Alec J Quiroga <aquiroga2@stu.edu>
2021-05-05 22:15:54 -0400

    Add files via upload

--- core/src/com/mygdx/game/bullet/Bullet.java
+++ core/src/com/mygdx/game/bullet/Bullet.java
@@ -1,3 +1,13 @@
+/**
+Copyright 2021 STU Computer Science
+
+An abstract class that contains the logic for the bullets.
+
+*/
+
+/**
+ * @author Alec Quiroga, Andhy Gomez
+ */
 package com.mygdx.game.bullet;
 
 import com.badlogic.gdx.Gdx;
@@ -7,18 +17,18 @@ import com.badlogic.gdx.math.Rectangle;
 import com.mygdx.game.TankGame;
 
 public class Bullet {
+	
+	// Declaring variables
 	public static final int SPEED = 10;
-	
 	private static Texture pellet;
-	
 	float x,y;
 	String direction;
 	public Rectangle bulletRect;
-	
 	public boolean remove = false;
-	
 	TankGame tank = new TankGame();
 	
+	
+	// Constructor that places the bullet on the screen and sets the direction it will hold.
 	public Bullet(float x, float y, String direction) {
 		this.x = x + 35;
 		this.y = y + 35;
@@ -30,6 +40,8 @@ public class Bullet {
 		}
 	}
 	
+	
+	// Updates the place the pellet is on the screen.
 	public void update() {
 		
 		
@@ -55,11 +67,13 @@ public class Bullet {
 		if (x > Gdx.graphics.getWidth() - 520 || x < 0) {
 			remove = true;
 		}
+		
+		// Generates the rectangle of the bullets.
 		bulletRect = new Rectangle(x, y, pellet.getWidth(), pellet.getHeight());
 
 	}
 	
-	
+	// Draws the pellets.
 	public void render(SpriteBatch batch) {
 		batch.draw(pellet,  x,  y);
 	}
